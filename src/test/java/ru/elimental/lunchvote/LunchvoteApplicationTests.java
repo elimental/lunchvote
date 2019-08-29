b87c9481c2b081fb3f79a5997a769c328d84f7b6
Dima <elimental@bk.ru>
2019-08-29 21:43:39 +0300

    init

---
+++ src/test/java/ru/elimental/lunchvote/LunchvoteApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.elimental.lunchvote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LunchvoteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
